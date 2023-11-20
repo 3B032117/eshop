2d7d6052eb86f92e3d9033263f9632d4eb31e4df
吳宇翰 <3b032117@gm.student.ncut.edu.tw>
2023-11-20 11:46:34 +0800

    Product CRUD路由註解

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// CRUD 路由使用的方法、URL及路由名稱
+// 
+// GET     Products                 Products.index
+// GET     Products/{Product}       Products.show
+// GET     Products/create          Products.create
+// POST    Products                 Products.store
+// GET     Products/{Product}/edit  Products.edit
+// PATCH   Products/{Product}       Products.update
+// DELETE  Products/{Product}       Products.destroy
+Route::resource('Products', ProductController::class);

121d2c3035e171923a3ca3e999b69eb97bde2807
吳宇翰 <3b032117@gm.student.ncut.edu.tw>
2023-11-20 11:52:24 +0800

    Product CRUD各路由作用註解

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,24 @@ Route::get('/', function () {
 // CRUD 路由使用的方法、URL及路由名稱
 // 
 // GET     Products                 Products.index
+// 作用: 顯示所有 Products 的資料
+// 
 // GET     Products/{Product}       Products.show
+// 作用: 顯示 Products 資料中的單一筆ID為 {Product} 的資料
+//
 // GET     Products/create          Products.create
+// 作用: 顯示用於建立新的 Product 的前端表單
+//
 // POST    Products                 Products.store
+// 作用: 將 Products/create 路由表單內容新增到資料庫
+//
 // GET     Products/{Product}/edit  Products.edit
+// 作用: 從資料庫查找單一筆ID為 {Product} 的資料，並顯示表單供編輯
+// 
 // PATCH   Products/{Product}       Products.update
+// 作用: 將 Products/{Product}/edit 路由所提供的單一筆ID為 {Product} 的資料更新到資料庫
+//
 // DELETE  Products/{Product}       Products.destroy
+// 作用: 刪除單一筆ID為 {Product} 的資料
+//
 Route::resource('Products', ProductController::class);

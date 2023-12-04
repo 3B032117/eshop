b35ac3cca3af21ccbcfb136ba59e895d81ecec9b
吳宇翰 <3b032117@gm.student.ncut.edu.tw>
2023-12-04 10:45:27 +0800

    安裝使用者身分驗證相關套件與功能(breeze)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -54,3 +55,15 @@ Route::post('Products', [ProductController::class, 'store']);
 Route::get('Products/{Product}/edit', [ProductController::class, 'edit']);
 Route::patch('Products/{Product}', [ProductController::class, 'update']);
 Route::delete('Products/{Product}', [ProductController::class, 'destroy']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
